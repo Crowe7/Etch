f62b64ce4b34cc050dfaa61ec6ab7580ba5480d2
Crowe7 <jacobocrowe@gmail.com>
2021-11-10 18:33:54 -0600

    added makeGrid function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let slider = document.querySelector('#myRange');
 let output = document.querySelectorAll('.gridAmount');
+let gridContainer = document.querySelector('#gridContainer');
 for (i=0; i < output.length; i++) {
     output[i].textContent = slider.value;
 }
@@ -8,6 +9,14 @@ slider.oninput = function() {
         output[i].textContent = this.value;
     }
 }
-
+function makeGrid(a) {
+    size = a;
+    for(i = 0; i < size; i++) {
+        for(j = 0; j < size; j++) {
+           let div = document.createElement('div');
+            gridContainer.appendChild(div);
+        }
+    }
+}
 // make a bunch of divs and give class fill <div class="fill"></div> like that
  
