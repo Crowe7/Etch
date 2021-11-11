e65a9732c6b74d8a76947fd0337913e42354263d
Crowe7 <jacobocrowe@gmail.com>
2021-11-11 06:45:47 -0600

    added the grid on startup

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 let slider = document.querySelector('#myRange');
 let output = document.querySelectorAll('.gridAmount');
 let gridContainer = document.querySelector('#gridContainer');
-for (i=0; i < output.length; i++) {
-    output[i].textContent = slider.value;
+let grid = document.querySelector(".grid");
+let gridCss = "grid-template:repeat(" +slider.value + ", 1fr) / repeat(" + slider.value + ", 1fr);"
+initPage();
+function initPage() {
+    for (i=0; i < output.length; i++) {
+        output[i].textContent = slider.value;
+    }
+    grid.style.cssText = gridCss;
+    makeGrid(slider.value);
 }
+
 slider.oninput = function() {
     for (i=0; i < output.length; i++) {
         output[i].textContent = this.value; 
     }
+    deleteGrid(this.value);
 }
 function makeGrid(a) {
     size = a;
     for(i = 0; i < size; i++) {
         for(j = 0; j < size; j++) {
-           let div = document.createElement('div');
+            let div = document.createElement('div');
             gridContainer.appendChild(div);
         }
     }
 }
-makeGrid(100);
 function deleteGrid(b) {
     size = b;
     for(i = 0; i < size; i++) {
         for(j = 0; j < size; j++) {
-           let div = gridContainer.lastElementChild;
+            let div = gridContainer.lastElementChild;
             div.remove();
         }
     }
 }
-deleteGrid(100);
+
 // make a bunch of divs and give class fill <div class="fill"></div> like that
- 
+// grid-template: repeat(16, 1fr) / repeat(16, 1fr);
