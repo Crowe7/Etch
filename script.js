c32aebfc6a9f01be5d84151027821b8b2358c44b
Crowe7 <jacobocrowe@gmail.com>
2021-11-12 06:45:30 -0600

    finished MVP

--- script.js
+++ script.js
@@ -3,15 +3,37 @@ let output = document.querySelectorAll('.gridAmount');
 let gridContainer = document.querySelector('#gridContainer');
 let grid = document.querySelector(".grid");
 let gridCss = 0;
+let onButton = 1;
+let gridDivs = gridContainer.children;
 initPage();
-function initPage() {
+ function initPage() {
     gridCss = "grid-template:repeat(" + slider.value + ", 1fr) / repeat(" + slider.value + ", 1fr);"
     for (i=0; i < output.length; i++) {
         output[i].textContent = slider.value;
     }
     grid.style.cssText = gridCss;
     makeGridDiv(slider.value);
+    addEventToDiv();
 }
+
+function addEventToDiv() {
+    for(i = 0; i < gridDivs.length; i++) {
+        gridDivs[i].addEventListener('mouseenter', function (e) {
+            if(onButton === 1) {
+                e.target.classList.add('fill');
+            }
+            else if(onButton === 2) {
+                e.target.classList.remove('fill');
+                e.target.style.cssText = pastelColors();
+            }
+            else if(onButton === 3) {
+                e.target.classList.remove('fill');
+                e.target.style.cssText = ""
+            }
+        });
+    }
+}
+
 
 slider.oninput = function() {
     for (i=0; i < output.length; i++) {
@@ -20,6 +42,7 @@ slider.oninput = function() {
 }
 slider.onchange = function() {
     makeGrid(slider.value, slider.value);
+    addEventToDiv();
 }
 function makeGridDiv(a) { 
     size = a;
@@ -50,31 +73,29 @@ function makeGrid(gridSize, gridCssChange) {
 }
 
 
-// make a bunch of divs and give class fill <div class="fill"></div> like that
-// grid-template: repeat(16, 1fr) / repeat(16, 1fr);
-// makeGrid(slider.value, slider.value);
-//const regBtn = document.querySelector('#regBtn');
 
-//regBtn.addEventListener('click', () => {
-    //regBtn.classList.add('regClass');
-//});
+// allows buttons to stay on when clicked/ and removes any other button thats active
  let buttonParent = document.querySelector('#buttons');
  buttonParent.addEventListener('click', function(e) {
      if(e.target && e.target.id === 'regBtn') {
         removeOtherButtonClasses();
         e.target.classList.add('regClass', 'active');
+        onButton = 1;
      }
      if(e.target && e.target.id === 'rainbowBtn') {
         removeOtherButtonClasses();
         e.target.classList.add('rainbowClass', 'active');
+        onButton = 2;
      }
      if( e.target && e.target.id === 'eraseBtn') {
         removeOtherButtonClasses();
         e.target.classList.add('eraseClass', 'active');
+        onButton = 3;
      }
       if(e.target && e.target.id === 'clearBtn') {
         removeOtherButtonClasses();
-        e.target.classList.add('clearClass', 'active');
+        makeGrid(slider.value, slider.value);
+        addEventToDiv();
      }
  });
 
@@ -84,6 +105,15 @@ function removeOtherButtonClasses() {
         if(currentButton[i].classList.contains('active')) {
             currentButton[i].className = '';
         }
+        else if(currentButton[i].classList.contains('regClass')) {
+            currentButton[i].className = '';
+        }
      }
 }
-// removeOtherButtonClasses();
+//got this from stackoverflow. generating 100% random colors was much worse looking
+function pastelColors(){
+    var r = (Math.round(Math.random()* 127) + 127).toString(16);
+    var g = (Math.round(Math.random()* 127) + 127).toString(16);
+    var b = (Math.round(Math.random()* 127) + 127).toString(16);
+    return 'background-color:' + '#' + r + g + b + ';';
+}
